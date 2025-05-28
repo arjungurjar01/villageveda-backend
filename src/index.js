2e9b3fd911c6d956fbb88db4525d292d15df7414
arjungurjar01 <arjunsinghgurjar781@gmail.com>
2025-05-28 16:28:37 +0530

    remove vercel configuration and update setup in src/index.js

--- src/index.js
+++ src/index.js
@@ -52,9 +52,20 @@ app.get("/api/health", (req, res) => {
 // })   
 
 
+connectDB()
+.then(()=>{
+    app.on("error",(error)=>{
+        console.log("Error : ",error);
+        throw error;
+       })
+
+    app.listen(process.env.PORT || 3000 , ()=>{
+        console.log(`Server is running locally on port ${process.env.PORT || 3000}`) 
+    })
+})
+.catch((err)=>{
+    console.log("MongoDB connection failed !!",err)
+}) 
 
 
 
-
-
-export default app;
--- src/test.js
+++ src/test.js
@@ -1,18 +1,18 @@
-import app from "../src/index.js"
-import connectDB from "./db/mongodb.js";
+// import app from "../src/index.js"
+// import connectDB from "./db/mongodb.js";
 
 
-connectDB()
-.then(()=>{
-    app.on("error",(error)=>{
-        console.log("Error : ",error);
-        throw error;
-       })
+// connectDB()
+// .then(()=>{
+//     app.on("error",(error)=>{
+//         console.log("Error : ",error);
+//         throw error;
+//        })
 
-    app.listen(process.env.PORT || 3000 , ()=>{
-        console.log(`Server is running locally on port ${process.env.PORT || 3000}`) 
-    })
-})
-.catch((err)=>{
-    console.log("MongoDB connection failed !!",err)
-})
+//     app.listen(process.env.PORT || 3000 , ()=>{
+//         console.log(`Server is running locally on port ${process.env.PORT || 3000}`) 
+//     })
+// })
+// .catch((err)=>{
+//     console.log("MongoDB connection failed !!",err)
+// })
